f7a0f19bb9a55ebdb68fc1ce638c9a569281b9a5
Thais Fernanda Da Silva Barros - SESI CUIABA-MT <thais.barros@alunosesi.mt>
2023-03-13 12:16:56 -0400

    segundo commit

---
+++ js/form.js
@@ -0,0 +1,54 @@
+let botaoAdicionar = document.querySelector('#adicionar-paciente')
+//adicionando evento ao botão click
+botaoAdicionar.addEventListener('click', function (evento){
+    //impede a natureza padrão do elemento
+    evento.preventDefault()
+
+    let form = document.querySelector('#form-adiciona')
+
+    let paciente = obtemPacienteDoForm(form)
+    
+    adicionaPacienteNaTabela(paciente)
+
+    form.reset()
+})
+
+function obtemPacienteDoForm(form){
+    let paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    return paciente
+}
+function adicionaPacienteNaTabela(paciente){
+    let pacienteTr = montarTr(paciente)
+    let tabela = document.querySelector('#tabela-pacientes')
+    tabela.appendChild(pacienteTr)
+    }
+
+function montarTr(paciente){
+
+    let pacienteTr = document.createElement('tr')
+    pacienteTr.classList.add('paciente')
+
+    pacienteTr.appendChild(montarTd(paciente.nome,"info-nome"))
+    pacienteTr.appendChild(montarTd(paciente.peso,"info-peso"))
+    pacienteTr.appendChild(montarTd(paciente.altura,"info-altura"))
+    pacienteTr.appendChild(montarTd(paciente.gordura,"info-gordura"))
+    pacienteTr.appendChild(montarTd(paciente.imc,"info-imc"))
+
+    return pacienteTr
+
+}
+
+function montarTd(dado, classe){
+
+    let td = document.createElement('td')
+    td.textContent = dado
+    td.classList.add(classe)
+
+    return td 
+}
